4496fa30607e13f92c1d28c1c730dd60c8e9dbfe
Matheus Falcirolli <fmatheus8@gmail.com>
2023-06-10 06:40:26 -0300

    Dapper: parameters and transaction

--- src/MSF.Util/Dapper/DapperDemo.cs
+++ src/MSF.Util/Dapper/DapperDemo.cs
@@ -1,5 +1,5 @@
 ﻿using Dapper;
-using System.Collections.Generic;
+using System;
 using System.Data.SqlClient;
 using System.Linq;
 
@@ -22,6 +22,48 @@ namespace MSF.Util.Dapper
                 var customers = connection.Query<Customer>(query).AsQueryable();
 
                 return customers;
+            }
+        }
+
+        public Customer Parameters()
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("CustomerId", 1);
+
+            using (var connection = new SqlConnection("data source=(localdb)\\MSSQLLocalDB;initial catalog=RestaurantDB;integrated security=True;MultipleActiveResultSets=True"))
+            {
+                connection.Open();
+
+                var query = "SELECT * FROM TB_CUSTOMER WHERE Id = @CustomerId";
+                var customer = connection.QueryFirstOrDefault<Customer>(query, parameters);
+
+                return customer;
+            }
+        }
+
+        public void Transactions()
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("CustomerId", 1);
+
+            using (var connection = new SqlConnection("connectionString"))
+            {
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        connection.Open();
+
+                        var query = "INSERT INTO TB_CUSTOMER (Id, Name) Values (3, 'Name')";
+                        connection.Execute(query);
+
+                        transaction.Commit();
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                    }
+                }
             }
         }
     }
