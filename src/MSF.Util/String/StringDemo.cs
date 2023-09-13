8f38681e3a593d26398e438b4faeee8c4eb38fca
Matheus Falcirolli <fmatheus8@gmail.com>
2023-09-13 18:23:26 -0300

    String vs string

---
+++ src/MSF.Util/String/StringDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSF.Util.StringDemo
+{
+    public class StringDemo
+    {
+        public void StringDemonstration()
+        {
+            // Use 'String' when
+            // 1. Working with Static Methods
+            String.Concat("Hello, ", "world!");
+            // 2. Fully Qualified name required in an environment where namespacing is an issua
+            String especificNamespaceConflitHere = String.Empty;
+
+            // Use 'string' when
+            // 1. Local variables and parameters
+            string greeting = "Hello";
+            // 2. Readability. Keeps your code consistent with the use of other built-in types
+            string one = "1";
+            int two = 2;
+        }
+    }
+}
