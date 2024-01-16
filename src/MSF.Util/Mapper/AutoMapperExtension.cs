78799c028eb3c1ac4b38c08dad0af374798f7889
Matheus Falcirolli <fmatheus8@gmail.com>
2024-01-16 15:35:52 -0300

    AutoMapper Extension

---
+++ src/MSF.Util/Mapper/AutoMapperExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSF.Util.Mapper
+{
+    public static class AutoMapperExtension
+    {
+        public static TDestination MapTo<TSource, TDestination>(this TSource source)
+        {
+            return AutoMapperConfiguration.Mapper.Map<TSource, TDestination>(source);
+        }
+
+        public static TDestination MapTo<TSource, TDestination>(this TSource source, TDestination destination)
+        {
+            return AutoMapperConfiguration.Mapper.Map<TSource, TDestination>(source, destination);
+        }
+    }
+}
